9e3bf7884b9d9c02cd04c3541a1dfc43160002fd
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-03-27 11:53:52 +0300

    add negative test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -17,6 +17,14 @@ import java.util.GregorianCalendar;
 
 public class CardDeliveryTest {
 
+    public String dataDelivery() {
+        Calendar calendar = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_YEAR, 4);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String dataDelivery = dateFormat.format(calendar.getTime());
+        return dataDelivery;
+    }
+
     @BeforeEach
     public void openBrowser() {
         open("http://localhost:9999/");
@@ -26,11 +34,7 @@ public class CardDeliveryTest {
     public void formSendSuccessWindowAppear(){
         $("[data-test-id=city] input").val("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_YEAR, 4);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dataDelivery = dateFormat.format(calendar.getTime());
-        $("[data-test-id=date] input").val(dataDelivery);
+        $("[data-test-id=date] input").val(dataDelivery());
         $("[data-test-id=name] input").val("Пушкин Алекандр");
         $("[data-test-id=phone] input").val("+79876543210");
         $("[data-test-id=agreement]").click();
@@ -42,11 +46,7 @@ public class CardDeliveryTest {
     public void formSendSuccessMessageSuccessfully(){
         $("[data-test-id=city] input").val("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_YEAR, 4);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dataDelivery = dateFormat.format(calendar.getTime());
-        $("[data-test-id=date] input").val(dataDelivery);
+        $("[data-test-id=date] input").val(dataDelivery());
         $("[data-test-id=name] input").val("Пушкин Алекандр");
         $("[data-test-id=phone] input").val("+79876543210");
         $("[data-test-id=agreement]").click();
@@ -61,18 +61,194 @@ public class CardDeliveryTest {
     public void formSendSuccessMessageDateDelivery(){
         $("[data-test-id=city] input").val("Москва");
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_YEAR, 4);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dataDelivery = dateFormat.format(calendar.getTime());
-        $("[data-test-id=date] input").val(dataDelivery);
+        $("[data-test-id=date] input").val(dataDelivery());
         $("[data-test-id=name] input").val("Пушкин Алекандр");
         $("[data-test-id=phone] input").val("+79876543210");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
         $("[data-test-id=notification] .notification__title").should(appear, Duration.ofSeconds(15));
         String actual =  $("[data-test-id=notification] .notification__content").getText();
-        String expected = "Встреча успешно забронирована на " + dataDelivery;
+        String expected = "Встреча успешно забронирована на " + dataDelivery();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void allNull(){
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=city].input_invalid .input__sub").getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cityNull(){
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val(dataDelivery());
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=phone] input").val("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=city].input_invalid .input__sub").getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void cityInvalid(){
+        $("[data-test-id=city] input").val("Обнинск");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val(dataDelivery());
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=phone] input").val("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=city].input_invalid .input__sub").getText();
+        String expected = "Доставка в выбранный город недоступна";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dateNull(){
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=phone] input").val("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=date] .input_invalid .input__sub").getText();
+        String expected = "Неверно введена дата";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dateIsOutdated(){
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val("01.01.2021");
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=phone] input").val("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=date] .input_invalid .input__sub").getText();
+        String expected = "Заказ на выбранную дату невозможен";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dateInvalid(){
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val("32.04.2022");
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=phone] input").val("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=date] .input_invalid .input__sub").getText();
+        String expected = "Неверно введена дата";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nameNull(){
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val(dataDelivery());
+        $("[data-test-id=phone] input").val("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=name].input_invalid .input__sub").getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nameInvalid(){
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val(dataDelivery());
+        $("[data-test-id=name] input").val("Пушкин Алекандр-2");
+        $("[data-test-id=phone] input").val("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=name].input_invalid .input__sub").getText();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void phoneNull() {
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val(dataDelivery());
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual = $("[data-test-id=phone].input_invalid .input__sub").getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void phoneInvalid() {
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val(dataDelivery());
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=phone] input").val("89876543210");
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual = $("[data-test-id=phone].input_invalid .input__sub").getText();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void allFieldsNullCheckboxChecked(){
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=agreement]").click();
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=city].input_invalid .input__sub").getText();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void allFieldsValidCheckboxUnchecked(){
+        $("[data-test-id=city] input").val("Москва");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val(dataDelivery());
+        $("[data-test-id=name] input").val("Пушкин Алекандр");
+        $("[data-test-id=phone] input").val("+79876543210");
+        $(withText("Забронировать")).click();
+        boolean actual = $("[data-test-id=agreement].input_invalid").isDisplayed();
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void allFieldsInvalidCheckboxChecked(){
+        $("[data-test-id=city] input").val("Обнинск");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val("32.04.2022");
+        $("[data-test-id=name] input").val("Пушкин Алекандр-2");
+        $("[data-test-id=phone] input").val("89876543210");
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=city].input_invalid .input__sub").getText();
+        String expected = "Доставка в выбранный город недоступна";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void allFieldsInvalidCheckboxUnchecked(){
+        $("[data-test-id=city] input").val("Обнинск");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id=date] input").val("32.04.2022");
+        $("[data-test-id=name] input").val("Пушкин Алекандр-2");
+        $("[data-test-id=phone] input").val("89876543210");
+        $(withText("Забронировать")).click();
+        String actual =  $("[data-test-id=city].input_invalid .input__sub").getText();
+        String expected = "Доставка в выбранный город недоступна";
         assertEquals(expected, actual);
     }
 }
